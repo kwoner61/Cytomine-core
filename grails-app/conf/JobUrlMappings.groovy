ec3a7c552acb02311cffd8383a2b1252e3b32021
13eMualliug <g.vissers13@gmail.com>
2019-05-10 15:23:51 +0200

    re-adding an url mapping... Careless deleted

--- grails-app/conf/JobUrlMappings.groovy
+++ grails-app/conf/JobUrlMappings.groovy
@@ -31,6 +31,9 @@ class JobUrlMappings {
         "/api/job/$id/execute.$format" (controller : "restJob") {
             action = [POST: "executeJobs", GET : "executeJobs"]
         }
+        "/api/job/execute.$format" (controller : "restJob") {
+            action = [POST: "executeJobs", GET : "executeJobs"]
+        }
 
         "/api/job/$job_id/processing_server/$processing_server_id/execute.$format" (controller: "restJob") {
             action = [POST: "executeWithProcessingServer", GET: "executeWithProcessingServer"]
